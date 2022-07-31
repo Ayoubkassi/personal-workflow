1c0136fa23cc2387ca1faf4e13b0b4ae9893c021
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-07-31 13:13:43 +0100

    fix issues

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const PORT = process.env.PORT || 5000
+// const express = require('express')
+// const cors = require('cors')
+// require('dotenv').config()
+// const PORT =  5000
 
-const app = express()
-app.use(cors())
+// const app = express()
+// app.use(cors())
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
 const sum = (a , b) => {
   return a+b;
 }
 
-app.get('/', (req, res) => {
-  res.send('Bismi Allah')
-})
+// app.get('/', (req, res) => {
+//   res.send('Bismi Allah')
+// })
 
-app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`App is listening on port ${PORT}`)
+// })
 
 module.exports = sum;
